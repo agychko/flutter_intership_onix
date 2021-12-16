76a0df057b680315579bbf7a7b5c85b3a3fb5136
agychko <agychko@gmail.com>
2021-12-17 00:01:55 +0200

    lesson_1 changes5

--- lib/data/models/wallet.dart
+++ lib/data/models/wallet.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: avoid_print
+
 import 'currency.dart';
 
 class Wallet extends Currency {
--- lib/main.dart
+++ lib/main.dart
@@ -32,13 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,11 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
