08462e2d726ba15e536020fe78a3cb5656f3689f
agychko <agychko@gmail.com>
2021-12-17 00:16:04 +0200

    lesson_1 changes5_1

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
@@ -55,7 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+         );
   }
 }
