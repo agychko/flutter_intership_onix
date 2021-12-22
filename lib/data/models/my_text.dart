6c29e97aa9d374d90ed1fd127369e07e5a43d513
agychko <agychko@gmail.com>
2021-12-22 21:16:19 +0200

    lesson_3 changes_Text_Container

--- lib/data/models/my_text.dart
+++ lib/data/models/my_text.dart
@@ -15,8 +15,20 @@ class MyTextState extends State<MyText> {
   }
   @override
   Widget build(BuildContext context) {
-    return Text (newText,
+    return Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.all(20),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+              side: const BorderSide(color:Colors.blue, width: 4.0, style: BorderStyle.solid)
+      ),
+      ),
+      child: Text (newText,
       style: Theme.of(context).textTheme.headline4,
+    ),
     );
   }
 }
