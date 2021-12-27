b1fa13f08ff1a1f86073df2a1d497d6d34f70e44
agychko <agychko@gmail.com>
2021-12-27 22:49:56 +0200

    lesson_4 converter_3

--- lib/data/models/my_text.dart
+++ lib/data/models/my_text.dart
@@ -30,8 +30,9 @@ class MyTextState extends State<MyText> {
           Text (newText,
         style: const TextStyle(color: Colors.blue,fontSize: 24,),
         ),
-          const SizedBox(height: 30),
+          const SizedBox(height: 20),
           TextField(
+            style: const TextStyle(color: Colors.black,fontSize: 20),
            decoration: InputDecoration(
              filled: true,
              fillColor: Colors.white12,
@@ -42,6 +43,7 @@ class MyTextState extends State<MyText> {
              border: OutlineInputBorder(borderRadius: BorderRadius.circular(25.0)),
              hintText: 'Search',
              hintStyle: const TextStyle(color: Colors.grey,fontSize: 20,),
+             contentPadding: const EdgeInsets.all(8.0),
            ),
       ),
           const SizedBox(height: 30),
@@ -75,32 +77,70 @@ class MyTextState extends State<MyText> {
               ),
             ),
             ),
-          const SizedBox(height: 30),
-          Card(
-            child: Column(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:[
-                ListTile(
-                leading: Image.network ('https://flagcdn.com/w160/us.jpg', width: 75, height: 100, fit:BoxFit.fill, ),
-                title: const Text('USD'),
-                subtitle: const Text('American Dollar'),
-                trailing: const Icon(Icons.arrow_forward_ios),
-                ),
-                const TextField(
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(color: Colors.black,fontSize: 28),
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.attach_money),
-                    hintText: '0.00',
-                    hintStyle: TextStyle(color: Colors.grey,fontSize: 28),
-                    contentPadding: EdgeInsets.all(20),
+              children: [
+                ElevatedButton(
+                    onPressed:() {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.grey,
+                    elevation: 20,
+                    padding: const EdgeInsets.all(5),
                   ),
+                    child: const Text('=',style: TextStyle(fontSize: 30)),
+                ),
+                ElevatedButton(
+                    onPressed:() {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.grey,
+                    elevation: 20,
+                    padding: const EdgeInsets.all(12),
+                  ),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.import_export),
+                        Text('Switch Currencies'),
+                      ],
+                    ),
                 ),
               ],
-              ),
-            margin: const EdgeInsets.all(20),
+            ),
+          ),
+          Card(
             color: Colors.white,
             elevation: 20,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:[
+                  ListTile(
+                  leading: Image.network ('https://flagcdn.com/w160/us.jpg', width: 75, height: 100, fit:BoxFit.fill, ),
+                  title: const Text('USD'),
+                  subtitle: const Text('American Dollar'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(color: Colors.black,fontSize: 28),
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.attach_money),
+                        hintText: '0.00',
+                        hintStyle: TextStyle(color: Colors.grey,fontSize: 28),
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+                    ),
+                  ),
+                ],
+                ),
+            ),
+
           ),
       ],
     ),
