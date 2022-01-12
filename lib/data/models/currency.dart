b21d39e66c2cc70a47ee61da422ec55faab1b652
agychko <agychko@gmail.com>
2022-01-12 20:11:42 +0200

    lesson_5 add flutter_svg_package

--- lib/data/models/currency.dart
+++ lib/data/models/currency.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 class Currency {
   var flag = Image.network('');
-  var icon = const Icon(Icons.import_export);
+  var icon = SvgPicture.asset('assets/symbol_euro_icon.svg', color: Colors.red);
   String name = "undefined";
   String symbol = "undefined";
   double rateToUah = 1;
@@ -9,14 +10,14 @@ class Currency {
   Currency();
   Currency.usd() {
     flag = Image.network('https://flagcdn.com/w80/us.png',fit: BoxFit.fill);
-    icon = const Icon(Icons.attach_money);
+    // icon = const Icon(Icons.attach_money);
     name = "Долар США";
     symbol = "USD";
     rateToUah = 27.3317;
   }
   Currency.euro() {
     flag = Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/255px-Flag_of_Europe.svg.png',fit: BoxFit.fill);
-    icon = const Icon(Icons.euro);
+    icon = SvgPicture.asset('assets/symbol_euro_icon.svg');
     name = "Євро";
     symbol = "EUR";
     rateToUah = 30.7277;
