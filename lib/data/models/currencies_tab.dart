33b273cb2e63b77ca1c221214ea88a693c18f71c
agychko <agychko@gmail.com>
2022-01-17 18:32:08 +0200

    lesson_6 add currency_repository

--- lib/data/models/currencies_tab.dart
+++ lib/data/models/currencies_tab.dart
@@ -1,4 +1,5 @@
-import 'package:first/data/models/currency.dart';
+
+
 import 'package:flutter/material.dart';
 
 class CurrenciesTab extends StatefulWidget {
@@ -10,29 +11,47 @@ class CurrenciesTab extends StatefulWidget {
 
 class CurrenciesTabState extends State<CurrenciesTab> {
 
-  List <Currency> currencies = [Currency.euro(),Currency.usd(),Currency.uah()];
+  // List <Currency> currencies = [Currency.euro(),Currency.usd(),Currency.uah()];
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      separatorBuilder: (context, index)=>const Divider(
-        color: Colors.grey, height: 40, thickness: 0.5, indent: 20, endIndent: 20,
-      ),
-        itemCount: currencies.length,
-        itemBuilder: (context, index){
-          return ListTile(
-            leading: SizedBox(
-              width: 70, height: 70,
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                child: currencies[index].flag,
-              ),
-            ),
-           title: Text(currencies[index].name),
-           subtitle: Text(currencies[index].symbol),
-            trailing: const Icon(Icons.arrow_forward_ios),
-          );
-        }
-    );
+    return const Text('');
+    //   return FutureBuilder (
+    //     future: _calculation,
+    //       builder: (BuildContext context, AsyncSnapshot <CurrencyRepository> snapshot) {
+    //           if (snapshot.hasError) {
+    //             return const Text('Error');
+    //           }
+    //           if (snapshot.hasData) {
+    //             // return ListView.separated(
+    //             //     separatorBuilder: (context, index)=>const Divider(
+    //             //       color: Colors.grey, height: 40, thickness: 0.5, indent: 20, endIndent: 20,
+    //             //     ),
+    //             //     itemCount: 3,
+    //             //     itemBuilder: (context, index){
+    //             //       return ListTile(
+    //             //         leading: SizedBox(
+    //             //           width: 70, height: 70,
+    //             //           child: Card(
+    //             //             clipBehavior: Clip.antiAlias,
+    //             //             child: Image.network(snapshot.data!.euro[0]),
+    //             //           ),
+    //             //         ),
+    //             //         title: Text(snapshot.data!.euro[1]),
+    //             //         subtitle: Text(snapshot.data!.euro[0]),
+    //             //         trailing: const Icon(Icons.arrow_forward_ios),
+    //             //       );
+    //                 // }
+    //             // );;
+    //           }
+    //           else {
+    //             return const Text('Loading...');
+    //           }
+    //       },
+    //   );
+    // }
+    // final Future<CurrencyRepository> _calculation = Future<CurrencyRepository>.delayed(const Duration(seconds: 2),
+    //       ()=> context,
+    // );
   }
-}
 
+}
---
+++ lib/data/models/currency_repository.dart
@@ -0,0 +1,33 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CurrencyRepository {
+
+  List flag = [
+    Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/255px-Flag_of_Europe.svg.png',fit: BoxFit.fill), //euro
+    Image.network('https://flagcdn.com/w80/us.png',fit: BoxFit.fill), //usd
+    Image.network('https://flagcdn.com/w80/ua.png',fit: BoxFit.fill) //uah
+  ];
+  List icon = [
+    SvgPicture.asset('assets/symbol_euro_icon.svg', color: const Color.fromRGBO(0, 60, 143, 1), width: 24, height: 24), //euro
+    SvgPicture.asset('assets/currency_dollar_fee_money_icon.svg', color: const Color.fromRGBO(0, 60, 143, 1), width: 24), //usd
+    SvgPicture.asset('assets/symbol_euro_icon.svg', color: const Color.fromRGBO(0, 60, 143, 1), width: 24, height: 24) //uah
+  ];
+  List name = [
+    "Euro",
+    "American Dollar",
+    "Ukrainian Hryvnia"
+  ];
+  List symbol = [
+    "EUR",
+    "USD",
+    "UAH"
+  ];
+  List rateToUah = [
+    30.7277, //euro
+    27.3317, //usd
+    1, //uah
+  ];
+}
